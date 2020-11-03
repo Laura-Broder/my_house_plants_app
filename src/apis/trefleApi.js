37b2d742b4db85b2ba7c63278af34e6cd2f3c8ff
Laura Broder <laura.kokielov@gmail.com>
2020-11-03 14:28:48 +0200

    remove proxy

--- src/apis/trefleApi.js
+++ src/apis/trefleApi.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://thingproxy.freeboard.io/fetch/https://trefle.io",
+  baseURL: "https://trefle.io",
   params: {
     token: "_4Q7DBGD5FqZdydDAgn-Vc6A5DXJShEcCvhfEmQd_OY",
   },
 });
-
+// _4Q7DBGD5FqZdydDAgn-Vc6A5DXJShEcCvhfEmQd_OY
+// https://thingproxy.freeboard.io/fetch/
 // https://trefle.io/api/v1/plants/search?token=_4Q7DBGD5FqZdydDAgn-Vc6A5DXJShEcCvhfEmQd_OY&page=2&q=rose
 // http://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=_4Q7DBGD5FqZdydDAgn-Vc6A5DXJShEcCvhfEmQd_OY&q=rose
